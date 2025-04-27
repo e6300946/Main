5f9947a5f9e2a0f1035d7d9c34ae4c9dabb80f88
Mael Métivier <e6300946@cmaisonneuve.qc.ca>
2025-04-26 22:28:36 -0400

    Ceci est le commit avec le problème de chemin de fichier

--- src/pyforum/bd.py
+++ src/pyforum/bd.py
@@ -1,31 +1,42 @@
+#début du programme
+#import des modules
+import json
+import csv
+
+#import des classes
 from pyforum.utilisateur import Utilisateur
+from pyforum.forum import Forum
 
 
 class BD:
+    """Cette classe permet de gérer la base de données du forum"""
+
     def __init__(self):
+        """Constructeur de la classe permettant de gérer les différents attributs de la classe base de données"""
         self.utilisateurs: list[Utilisateur] = []
-        self.forums = []
+        self.forums: list[Forum] = []
         self.publications = []
         self.commentaires = []
         self.utilisateurs_forums = {}
         print("Base de données initialisée.")
 
-    def creer_utilisateur(self, username: str) -> Utilisateur:
+    def creer_utilisateur(self, username:str, adresseCourriel: str, motDePasse: str, listeForums: list) -> Utilisateur:
+        """Méthode permettant de créer un objet utilisateur, générer son identifiant unique et retourner l'objet créé"""
         #                       ^^^^^^^^^^^^^^
         #            # TODO:    Vous devez ajouter les autres paramètres requis
 
         # Vérifier si l'utilisateur existe déjà
         if username in [u.username for u in self.utilisateurs]:
             print(f"[Simulé] L'utilisateur {username} existe déjà.")
-            return
+            
 
         # Créer un nouvel identifiant pour l'utilisateur
         new_id = max([u.id for u in self.utilisateurs], default=0) + 1
 
         # Instancier un nouvel utilisateur et l'ajouter à la liste
-        u = Utilisateur(new_id, username)
+        u = Utilisateur(new_id, username, adresseCourriel, motDePasse, listeForums = [])
         self.utilisateurs.append(u)
-        print(f"[Simulé] Sauvegarde de l'utilisateur: {u}")
+        print(f"[Simulé] Sauvegarde du nouveau utilisateur: {u}")
 
         # Retourner l'utilisateur créé
         return u
@@ -34,12 +45,28 @@ class BD:
         for u in self.utilisateurs:
             if u.username == nom_utilisateur:
                 return u
+    
+    def sauvegardeDutilisateurs(self):
+        with open('src/pyforum/data/utilisateurs.json', 'r', encoding='utf-8') as fichier:
+            data = json.load(fichier)
+        
+        
+        nouvelutilisateur = BD.creer_utilisateur(username="toto", adresseCourriel="toto@gmail.com", motDePasse="loltruc", listeForums=["animeau","finances","voitures"])
+        data.append(nouvelutilisateur.to_dict())
+        with open('src/pyforum/datautilisateurs.jsonn', 'w', encoding='utf-8') as fichier:
+            json.dump(data, fichier, ensure_ascii = False, indent = 4)
 
-    def creer_forum(self, nom):
+bd = BD()
+bd.sauvegardeDutilisateurs()      
+
+'''
+    def creer_forum(self, nom: str, id: str, listePublications: list, description: str) -> Forum:
         #                ^^^^^^
         #                Vous devez ajouter les autres paramètres requis
         # TODO: Implanter la logique pour créer un forum
-        pass
+        if nom in [f.nom for f in self.forums]:
+            print(f"[Simulé] Le forum {nom} existe déjà.")
+            return
 
     def creer_publication(self, publication):
         #                       ^^^^^^^^^^^
@@ -66,3 +93,4 @@ class BD:
         #                         Vous devez ajouter les autres paramètres requis
         # TODO: Implanter la logique pour mettre à jour le forum et retourner le forum mis à jour
         pass
+'''
--- src/pyforum/utilisateur.py
+++ src/pyforum/utilisateur.py
@@ -11,3 +11,6 @@ class Utilisateur():
     def __str__(self):
         """Méthode permettant d'afficher en format lisible les attributs de la classe utilisateur"""
         return f"Utilisateur(id={self.id}, username='{self.username}', adresse courriel='{self.adresseCourriel}', mot de passe='{self.motDePasse}')"
+    
+    def to_dict(self):
+        return {"id":self.id, "username":self.username, "adresse Courriel":self.adresseCourriel, "motDePasse":self.motDePasse, "liste de forums":self.listeForums}
